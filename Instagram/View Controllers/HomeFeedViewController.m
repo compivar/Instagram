01272eae45f8f83e1bddd9a701a1baa24e7c437a
compivar <13isaacvarsan@gmail.com>
2022-06-27 18:17:45 -0700

    Implemented Logout and User persistence features

--- Instagram/View Controllers/HomeFeedViewController.m
+++ Instagram/View Controllers/HomeFeedViewController.m
@@ -6,6 +6,10 @@
 //
 
 #import "HomeFeedViewController.h"
+#import "SceneDelegate.h"
+#import "Parse/Parse.h"
+#import "LoginViewController.h"
+
 
 @interface HomeFeedViewController ()
 
@@ -16,6 +20,17 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+}
+- (IBAction)didTapLogout:(id)sender {
+    SceneDelegate *mySceneDelegate = (SceneDelegate * ) UIApplication.sharedApplication.connectedScenes.allObjects.firstObject.delegate;
+
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    LoginViewController *loginViewController = [storyboard instantiateViewControllerWithIdentifier:@"LoginViewController"];
+    mySceneDelegate.window.rootViewController = loginViewController;
+    [PFUser logOutInBackgroundWithBlock:^(NSError * _Nullable error) {
+        // PFUser.current() will now be nil
+    }];
+    
 }
 
 /*
